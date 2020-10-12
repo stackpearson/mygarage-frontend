d9a2dd4323aad2a8807f6f72dd5ac12d5883c41c
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-10-12 14:44:53 -0600

    added drop down card for services

---
+++ src/components/Service.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Service = (props) => {
+
+    return (<>
+
+            <h3>{props.service.service_name}</h3>
+            <div className='service-container'>
+                <div className='service-pairs'>
+                    <p>Mileage performed: <span className='service-entry'>{props.service.service_mileage}</span></p>
+                    <p>Mileage due next: <span className='service-entry'>{props.service.next_service_mileage}</span></p>
+                    <p>Date of service: <span className='service-entry'>{props.service.service_date}</span></p>
+                    <p>Next service date: <span className='service-entry'>{props.service.next_service_date}</span></p>
+                </div>
+                <p>notes: {props.service.service_notes}</p>
+            </div>
+
+
+    </>)
+}
+
+export default Service;
--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -1,23 +1,48 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {Accordion, Card, Button} from 'react-bootstrap';
+import Service from './Service';
 
 
 const Vehicle = (props) => {
     let filteredServices = props.services.filter(services => services.id = props.vehicle.id)
-    const [vehicleServices] = useState(filteredServices);
+    
 
     
 
 
     return(<> 
 
+     
             <div className='mapped-vehicle'>
-                <div className='vehicle-interface'>
-                    
-                    <div className='vehicle'><h3>{props.vehicle.vehicle_year} {props.vehicle.vehicle_make} {props.vehicle.vehicle_model}</h3></div>
-                    <div className='service'><Link className='undecorated-link car' to={`/add-service/${props.vehicle.id}`}>+ Service</Link></div>
-                </div>              
+                <Accordion>
+                    <Card>
+                        <Card.Header>
+                            <div className='vehicle-interface'>
+                                <div className='vehicle'><h3>{props.vehicle.vehicle_year} {props.vehicle.vehicle_make} {props.vehicle.vehicle_model}</h3></div>
+                                <div className='service'><Link className='undecorated-link car' to={`/add-service/${props.vehicle.id}`}>+ Service</Link></div>
+                            </div>
+                            
+                        <Accordion.Toggle as={Button} variant="text" eventKey="0">
+                            View Services
+                        </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            {props.services.map(service => {
+                                return (
+                                        <div className='services-container'>
+                                            <Service key={service.id} service={service} />
+                                        </div>
+                                )
+                            })}
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
             </div>
+           
+
     
     </>)
 
