7398905b08190c0bcc08b39efcf3530553f1025f
Sawyer <sawyer.pearson92@gmail.com>
2022-01-16 23:12:30 -0700

    added a loading spinner to the registration page

--- src/Register.js
+++ src/Register.js
@@ -12,6 +12,8 @@ const [formState, setFormState] = useState({
     password:''
 });
 
+const [isRegistering, setRegister] = useState(true);
+
 const handleChange = (e) => {
     e.persist();
     const newFormState = {
@@ -22,6 +24,7 @@ const handleChange = (e) => {
 }
 
 const registerUser = (e) => {
+    setRegister(true)
     e.preventDefault();
     e.persist();
 
@@ -31,35 +34,51 @@ const registerUser = (e) => {
         console.log(res)
         localStorage.setItem('auth-token', res.data.token)
         localStorage.setItem('userId', res.data.id)
+        setRegister(false)
         props.history.push('/dashboard')
     })
 }
 
     return (<> 
-    
-        <div className='form-container'>
-            <Form className='form' onSubmit={registerUser}>
 
-                <div className='form-title'>Register</div>
-            
-                <Form.Group>
-                    <Form.Label className='form-label'>Username</Form.Label>
-                    <Form.Control type="text" name='username' value={formState.username} onChange={handleChange} placeholder="Enter username" />
-                </Form.Group>
+        {isRegistering ? (
 
-                <Form.Group>
-                    <Form.Label className='form-label'>Password</Form.Label>
-                    <Form.Control type="password" name='password' value={formState.password} onChange={handleChange} placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                <br/>
-                <div className='form-link'>
-                    <Link className='form-link' to='/'>Already Registered? Sign In Here</Link>
+            <div className='spinner-container'>
+                <div className="spinner-border" role='status' >
+                    <span className='visually-hidden'></span>
                 </div>
-            </Form>
-        </div>
+                <div>Initial registration can take up to a minute, please bear with us :)</div>
+            </div>
+
+        ) : (
+
+
+
+    
+            <div className='form-container'>
+                <Form className='form' onSubmit={registerUser}>
+
+                    <div className='form-title'>Register</div>
+                
+                    <Form.Group>
+                        <Form.Label className='form-label'>Username</Form.Label>
+                        <Form.Control type="text" name='username' value={formState.username} onChange={handleChange} placeholder="Enter username" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label className='form-label'>Password</Form.Label>
+                        <Form.Control type="password" name='password' value={formState.password} onChange={handleChange} placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    <br/>
+                    <div className='form-link'>
+                        <Link className='form-link' to='/'>Already Registered? Sign In Here</Link>
+                    </div>
+                </Form>
+            </div>
+            )}
     
     </>);
 
